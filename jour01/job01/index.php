38099c00ba42901cc81809de8a6ae16e4e1b95b4
Pierre-ninfei <pierre.ninfei@laplateforme.io>
2022-03-14 10:21:23 +0100

    added job01

---
+++ jour01/job01/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>
+
+	</title>
+	<script type="text/javascript">
+		function popup(){
+			alert("Hello Javascript !")
+		}
+	</script>
+</head>
+<body>
+	<button onclick="popup()">Popup</button>
+</body>
+</html>
