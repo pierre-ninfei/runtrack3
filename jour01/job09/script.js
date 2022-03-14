3a9618d4876c07a715753545255a11ce106079e1
Pierre-ninfei <pierre.ninfei@laplateforme.io>
2022-03-14 13:39:11 +0100

    added job09

---
+++ jour01/job09/script.js
@@ -0,0 +1,36 @@
+function tri(numbers, order){
+	var done = false;
+
+
+  	while(!done){
+	    done = true;
+	    if(order == "desc"){
+	    	for(var i = 1; i < numbers.length; i++){
+		    	if(numbers[i - 1] < numbers[i]){
+			        done = false;
+			        var tmp = numbers[i - 1];
+			        numbers[i - 1] = numbers[i];
+			        numbers[i] = tmp;
+		    	}
+	    	}
+	    }
+	    else if(order == "asc"){
+		    for(var i = 1; i < numbers.length; i++){
+		    	if(numbers[i - 1] > numbers[i]){
+			        done = false;
+			        var tmp = numbers[i - 1];
+			        numbers[i - 1] = numbers[i];
+			        numbers[i] = tmp;
+		    	}
+	    	}
+	    }
+  	}
+  	for( x = 0; x < numbers.length; x++){
+  		console.log(numbers[x])
+  	}
+}
+
+var order = prompt("Entrez une valeur (SOIT asc, SOIT desc UNIQUEMENT")
+var numbers = [1, 10, 11, 7, 21, 31, 37, 80, 7, 41]
+tri(numbers, order)
+console.log(order)
