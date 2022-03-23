f5d5c15e74e0961b8a1c8c6f656eb574ee189792
Pierre-ninfei <pierre.ninfei@laplateforme.io>
2022-03-23 13:36:47 +0100

    added job01

---
+++ jour03/job01/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+	<script type="text/javascript" src="script.js"></script>
+	<title>Runtrack3 - jour 3</title>
+</head>
+
+<body>
+	<button>Toggle text</button>
+	<p>le $ est un bon élément monétaire. Le # de twitter est une bonne I D. Il faudra faire le point avec la classe pour cacher cet élément.</p>
+</body>
+</html>
